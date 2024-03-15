17338a28e10c9897ab7f571d8316a057c3341891
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-15 16:29:59 +0530

    feat: Updated code for home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,17 @@
+import LottieView from 'lottie-react-native';
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 
 const Home = ({navigation}: any) => {
   return (
     <View style={styles.container}>
+      <LottieView
+        source={require('../assets/home.json')}
+        style={styles.lottieContainer}
+        resizeMode="cover"
+        autoPlay
+        loop={false}
+      />
       <Text style={styles.text}>Welcome to the Home Screen!</Text>
       <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
@@ -20,6 +28,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 20,
   },
+  lottieContainer: {width: 250, height: 250},
 });
 
 export default Home;
